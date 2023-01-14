829abf5a00c889b7b1f7e82e7a6e94d55911675d
shoe <69857862+FocaNTG@users.noreply.github.com>
2023-01-14 12:43:13 +0200

    Update nya-hunb.lua

--- nya-hunb.lua
+++ nya-hunb.lua
@@ -40,30 +40,6 @@ b:Toggle("Toggle",function(bool)
     print(shared.toggle)
 end)
 
-b:Slider("Slider",{
-    min = 10; -- min value of the slider
-    max = 50; -- max value of the slider
-    precise = true; -- max 2 decimals
-},function(value)
-    print(value)
-end)
-
-b:Dropdown("Dropdown",{"A","B","C"},true,function(mob) --true/false, replaces the current title "Dropdown" with the option that t
-    print(mob)
-end)
-
-b:Bind("Bind",Enum.KeyCode.C,function() --Default bind
-    print("Yes")
-end)
-
-b:ColorPicker("ColorPicker",Color3.fromRGB(255,0,0),function(color) --Default color
-    print(color)
-end)
-
-b:Box("Box","number",function(value) -- "number" or "string"
-    print(value)
-end)
-
 local g = w:CreateFolder("Fun")
 
 g:Button("R15 Animations",function()
