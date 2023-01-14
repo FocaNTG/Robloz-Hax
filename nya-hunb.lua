278b3a783e5e5a8c83252ec80fc2541c06101670
shoe <69857862+FocaNTG@users.noreply.github.com>
2023-01-14 16:18:32 +0200

    Update nya-hunb.lua

--- nya-hunb.lua
+++ nya-hunb.lua
@@ -22,6 +22,10 @@ b:Button("R15 Animations",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Roblox-Stuff/main/folder%20for%20scripts%20i%20uploaded/443244_source.lua"))()
 end)
 
+b:Button("Infinite Yield",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/iy.lua"))()
+end)
+
 local g = w:CreateFolder("Fun")
 
 g:Button("R15 Animations",function()
