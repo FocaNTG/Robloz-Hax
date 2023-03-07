da3f6fa96936806edc29b562aff1206d3ea1400d
shoe <69857862+FocaNTG@users.noreply.github.com>
2023-03-07 14:17:54 +0200

    Update nya-hunb.lua

--- nya-hunb.lua
+++ nya-hunb.lua
@@ -22,6 +22,10 @@ b:Button("CMD X",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/iy.lua"))()
 end)
 
+b:Button("Infinite Yield",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/actual_iy.lua"))()
+end)
+
 b:Button("BTools",function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/btools.lua"))()
 end)
