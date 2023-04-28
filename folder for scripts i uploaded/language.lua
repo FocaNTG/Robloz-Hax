750012b6eb26f5229d78708b3f7e144a5775c95c
shoe <69857862+FocaNTG@users.noreply.github.com>
2023-04-28 08:33:42 +0300

    Update language.lua

--- folder for scripts i uploaded/language.lua
+++ folder for scripts i uploaded/language.lua
@@ -1,108 +1,31 @@
-wait(10)
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/x114/RobloxScripts/main/UpdatedChatTranslator"))()
---[[ local languages = {
-        auto = "Automatic",
-        af = "Afrikaans",
-        sq = "Albanian",
-        am = "Amharic",
-        ar = "Arabic",
-        hy = "Armenian",
-        az = "Azerbaijani",
-        eu = "Basque",
-        be = "Belarusian",
-        bn = "Bengali",
-        bs = "Bosnian",
-        bg = "Bulgarian",
-        ca = "Catalan",
-        ceb = "Cebuano",
-        ny = "Chichewa",
-        ['zh-cn'] = "Chinese Simplified",
-        ['zh-tw'] = "Chinese Traditional",
-        co = "Corsican",
-        hr = "Croatian",
-        cs = "Czech",
-        da = "Danish",
-        nl = "Dutch",
-        en = "English",
-        eo = "Esperanto",
-        et = "Estonian",
-        tl = "Filipino",
-        fi = "Finnish",
-        fr = "French",
-        fy = "Frisian",
-        gl = "Galician",
-        ka = "Georgian",
-        de = "German",
-        el = "Greek",
-        gu = "Gujarati",
-        ht = "Haitian Creole",
-        ha = "Hausa",
-        haw = "Hawaiian",
-        iw = "Hebrew",
-        hi = "Hindi",
-        hmn = "Hmong",
-        hu = "Hungarian",
-        is = "Icelandic",
-        ig = "Igbo",
-        id = "Indonesian",
-        ga = "Irish",
-        it = "Italian",
-        ja = "Japanese",
-        jw = "Javanese",
-        kn = "Kannada",
-        kk = "Kazakh",
-        km = "Khmer",
-        ko = "Korean",
-        ku = "Kurdish (Kurmanji)",
-        ky = "Kyrgyz",
-        lo = "Lao",
-        la = "Latin",
-        lv = "Latvian",
-        lt = "Lithuanian",
-        lb = "Luxembourgish",
-        mk = "Macedonian",
-        mg = "Malagasy",
-        ms = "Malay",
-        ml = "Malayalam",
-        mt = "Maltese",
-        mi = "Maori",
-        mr = "Marathi",
-        mn = "Mongolian",
-        my = "Myanmar (Burmese)",
-        ne = "Nepali",
-        no = "Norwegian",
-        ps = "Pashto",
-        fa = "Persian",
-        pl = "Polish",
-        pt = "Portuguese",
-        pa = "Punjabi",
-        ro = "Romanian",
-        ru = "Russian",
-        sm = "Samoan",
-        gd = "Scots Gaelic",
-        sr = "Serbian",
-        st = "Sesotho",
-        sn = "Shona",
-        sd = "Sindhi",
-        si = "Sinhala",
-        sk = "Slovak",
-        sl = "Slovenian",
-        so = "Somali",
-        es = "Spanish",
-        su = "Sundanese",
-        sw = "Swahili",
-        sv = "Swedish",
-        tg = "Tajik",
-        ta = "Tamil",
-        te = "Telugu",
-        th = "Thai",
-        tr = "Turkish",
-        uk = "Ukrainian",
-        ur = "Urdu",
-        uz = "Uzbek",
-        vi = "Vietnamese",
-        cy = "Welsh",
-        xh = "Xhosa",
-        yi = "Yiddish",
-        yo = "Yoruba",
-        zu = "Zulu"]]
+pcall(function()
+  loadstring(
+      --[[
+
+      ░░░░░██╗  ███╗░░░███╗███████╗███╗░░░███╗███████╗
+      ░░░░░██║  ████╗░████║██╔════╝████╗░████║██╔════╝
+      ░░░░░██║  ██╔████╔██║█████╗░░██╔████╔██║█████╗░░
+      ██╗░░██║  ██║╚██╔╝██║██╔══╝░░██║╚██╔╝██║██╔══╝░░
+      ╚█████╔╝  ██║░╚═╝░██║███████╗██║░╚═╝░██║███████╗
+      ░╚════╝░  ╚═╝░░░░░╚═╝╚══════╝╚═╝░░░░░╚═╝╚══════╝
+
+
+      Keybind:
+     
+      - Semicolon // To trigger chat box
+      - Enter // To submit your chat
+      - Click on player chat // To view his translation
+      - Quote // To hide/show UI
+     
+      Next Update:
+     
+      - Ill make it so it auto detects language of chat upon click
+      - yes thats it
+      --]]
+
+      game:HttpGet(
+          'https://raw.githubusercontent.com/PivotEngine/cool_scripts/main/Pedro.exe',
+          true
+      )
+  )();
+end)
