cfb72576399895cac550c05b7f271bc2219bce36
shoe <69857862+FocaNTG@users.noreply.github.com>
2023-05-04 14:42:29 +0300

    Update nya-hunb.lua

--- nya-hunb.lua
+++ nya-hunb.lua
@@ -4,8 +4,4 @@ if ui_number_here = "1" then
 elseif ui_number_here = "2" then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/Ui2.lua"))()
 
-elseif ui_number_here = "3" then
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/Ui3.lua"))()
-
 end
