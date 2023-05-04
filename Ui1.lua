a090c40d01b9d9b8ca9164d5b55ae95ea68a066c
shoe <69857862+FocaNTG@users.noreply.github.com>
2023-05-04 11:17:55 +0300

    Create Ui1.lua

---
+++ Ui1.lua
@@ -0,0 +1,201 @@
+-- Gui stuff Made by : https://v3rmillion.net/member.php?action=profile&uid=507120 
+
+  
+
+ local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))() 
+
+  
+
+ local w = library:CreateWindow("nya hub") -- Creates the window 
+
+  
+
+ local b = w:CreateFolder("Utility") -- Creates the folder(U will put here your buttons,etc) 
+
+  
+
+ b:Button("SimpleSpy",function() 
+
+     loadstring(game:HttpGet(('https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua')))() 
+
+ end) 
+
+  
+
+ b:Button("Chat Translator",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/language.lua"))() 
+
+ end) 
+
+  
+
+ b:Button("Anti-Afk",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))() 
+
+ end) 
+
+  
+
+ b:Button("R15 Animations",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Roblox-Stuff/main/folder%20for%20scripts%20i%20uploaded/anim"))() 
+
+ end) 
+
+  
+
+ b:Button("CMD X",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/iy.lua"))() 
+
+ end) 
+
+  
+
+ b:Button("Infinite Yield",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/actual_iy.lua"))() 
+
+ end) 
+
+  
+
+ b:Button("Nameless Admin",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/nameless.lua"))() 
+
+ end) 
+
+  
+
+ b:Button("BTools",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/btools.lua"))() 
+
+ end) 
+
+  
+
+ b:Button("Server lagger",function() 
+
+     loadstring(game:HttpGet(('https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/crash.lua')))() 
+
+ end) 
+
+  
+
+ local g = w:CreateFolder("Fun") 
+
+  
+
+ g:Button("R15 Animations",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Roblox-Stuff/main/folder%20for%20scripts%20i%20uploaded/443244_source.lua"))() 
+
+ end) 
+
+  
+
+ g:Button("Pendulum (sussy owo)",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/pendd22.lua"))() 
+
+ end) 
+
+  
+
+ g:Button("2016 Roblox Ui",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/2016ui.lua"))() 
+
+ end) 
+
+  
+
+ g:Button("Rush Fling",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/343.lua"))() 
+
+ end) 
+
+  
+
+ b:DestroyGui() 
+
+  
+
+ local j1 = w:CreateFolder("Games") 
+
+  
+
+ j1:Button("MM2 Vynixu",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/mm2%20vynixu.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Arsenal easy",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Roblox-Stuff/main/folder%20for%20scripts%20i%20uploaded/44_source.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Arsenal no gui",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/44%20no%20gui.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Flee da Facility",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/flee%20the%20facc.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Big Paintball",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/big_paintball.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Survival Game",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/survival.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Build Boat",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/buildboat.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Mortem Metallum",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/mortemmetallum.lua"))() 
+
+ end) 
+
+  
+
+ j1:Button("Aimblox",function() 
+
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/FocaNTG/Robloz-Hax/main/folder%20for%20scripts%20i%20uploaded/aimblox"))() 
+
+ end)
